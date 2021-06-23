0df068a30ed4a90bf2c0b4ceeffa59a20c9d8f5c
Thomas Jakobi <thomas.jakobi@partout.info>
2021-06-23 14:45:11 +0200

    Update setting.inc.php

--- core/components/langsuggest/lexicon/de/setting.inc.php
+++ core/components/langsuggest/lexicon/de/setting.inc.php
@@ -5,6 +5,8 @@
  * @package langsuggest
  * @subpackage lexicon
  */
+$_lang['setting_langsuggest.chunk_position_desc'] = 'Die Position der Ausgabe des LangSuggest-Chunks im Body-Bereich der gerenderten Seite. Kann auf \'top\' und \'bottom\' gesetzt werden.';
+$_lang['setting_langsuggest.chunk_position'] = 'Chunk-Position';
 $_lang['setting_langsuggest.cookie_expiration'] = 'Cookie-Ablauf';
 $_lang['setting_langsuggest.cookie_expiration_desc'] = 'Die Anzahl der Tage, die der LangSuggest nicht sichtbar ist, wenn er durch die Schaltfläche geschlossen wird';
 $_lang['setting_langsuggest.cookie_name'] = 'Cookie Name';
@@ -17,11 +19,3 @@ $_lang['setting_langsuggest.no_session'] = 'Sessionslose Kontexte';
 $_lang['setting_langsuggest.no_session_desc'] = 'LangSuggest in sessionslosen Kontexten anzeigen';
 $_lang['setting_langsuggest.tpl'] = 'Modal Template';
 $_lang['setting_langsuggest.tpl_desc'] = 'Der Template-Chunk, der das LangSuggest-Modal enthält.';
-$_lang['setting_langsuggest.chunk_position_desc'] = 'Die Position der Ausgabe des LangSuggest-Chunks im Body-Bereich der gerenderten Seite. Kann auf \'top\' und \'bottom\' gesetzt werden.';
-/**
- * Setting Lexicon Entries for LangSuggest
- *
- * @package langsuggest
- * @subpackage lexicon
- */
-$_lang['setting_langsuggest.chunk_position'] = 'Chunk-Position';
