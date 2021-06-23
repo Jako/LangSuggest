57c84fe18ad6dbe5ee27bf2e1f928244cbdf2e09
Thomas Jakobi <buero@treehillstudio.de>
2021-06-23 12:41:53 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: MODX Extras/MODX LangSuggest/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-system-settings/de/

--- core/components/langsuggest/lexicon/de/setting.inc.php
+++ core/components/langsuggest/lexicon/de/setting.inc.php
@@ -5,15 +5,23 @@
  * @package langsuggest
  * @subpackage lexicon
  */
-$_lang['setting_langsuggest.cookie_expiration'] = 'Cookie Expiration';
-$_lang['setting_langsuggest.cookie_expiration_desc'] = 'The number of days the LangSuggest won\'t be visible, if it is closed by the button';
+$_lang['setting_langsuggest.cookie_expiration'] = 'Cookie-Ablauf';
+$_lang['setting_langsuggest.cookie_expiration_desc'] = 'Die Anzahl der Tage, die der LangSuggest nicht sichtbar ist, wenn er durch die Schaltfläche geschlossen wird';
 $_lang['setting_langsuggest.cookie_name'] = 'Cookie Name';
-$_lang['setting_langsuggest.cookie_name_desc'] = 'The name of the cookie that is set by LangSuggest, when the language should be switched.';
+$_lang['setting_langsuggest.cookie_name_desc'] = 'Der Name des Cookies, das von LangSuggest festgelegt wird, wenn die Sprache gewechselt werden soll.';
 $_lang['setting_langsuggest.debug'] = 'Debug';
 $_lang['setting_langsuggest.debug_desc'] = 'Log debug information in MODX error log.';
-$_lang['setting_langsuggest.display_count'] = 'Display Count';
-$_lang['setting_langsuggest.display_count_desc'] = 'Maximum display count of LangSuggest modal for each session, if a PHP session exist.';
-$_lang['setting_langsuggest.no_session'] = 'Sesssion-less Contexts';
-$_lang['setting_langsuggest.no_session_desc'] = 'Show LangSuggest on session-less contexts';
+$_lang['setting_langsuggest.display_count'] = 'Anzahl der Meldungen';
+$_lang['setting_langsuggest.display_count_desc'] = 'Maximale Anzahl der Meldungen des LangSuggest-Modals für jede Sitzung, wenn eine PHP-Sitzung existiert.';
+$_lang['setting_langsuggest.no_session'] = 'Sessionslose Kontexte';
+$_lang['setting_langsuggest.no_session_desc'] = 'LangSuggest in sessionslosen Kontexten anzeigen';
 $_lang['setting_langsuggest.tpl'] = 'Modal Template';
-$_lang['setting_langsuggest.tpl_desc'] = 'The template chunk containing the LangSuggest modal.';
+$_lang['setting_langsuggest.tpl_desc'] = 'Der Template-Chunk, der das LangSuggest-Modal enthält.';
+$_lang['setting_langsuggest.chunk_position_desc'] = 'Die Position der Ausgabe des LangSuggest-Chunks im Body-Bereich der gerenderten Seite. Kann auf \'top\' und \'bottom\' gesetzt werden.';
+/**
+ * Setting Lexicon Entries for LangSuggest
+ *
+ * @package langsuggest
+ * @subpackage lexicon
+ */
+$_lang['setting_langsuggest.chunk_position'] = 'Chunk-Position';
