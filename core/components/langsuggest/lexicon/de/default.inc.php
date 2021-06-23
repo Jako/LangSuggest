6a5762f7b0ae5fc5ab087c4f8f938e61792c7f46
Thomas Jakobi <buero@treehillstudio.de>
2021-06-23 12:43:15 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/de/

--- core/components/langsuggest/lexicon/de/default.inc.php
+++ core/components/langsuggest/lexicon/de/default.inc.php
@@ -1,3 +1,3 @@
 <?php
 $_lang['langsuggest.redirect_title'] = 'Kurz nachgefragt …';
-$_lang['langsuggest.redirect_message'] = 'Möchten Sie auf unsere deutschsprachigen Seite weitergeleitet werden?';
+$_lang['langsuggest.redirect_message'] = 'Möchten Sie auf unsere deutschsprachige Seite weitergeleitet werden?';
