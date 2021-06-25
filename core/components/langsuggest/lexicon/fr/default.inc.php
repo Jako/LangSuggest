416a0c452f5997a5331614ce64e3512a7f8c8a13
Thomas Jakobi <buero@treehillstudio.de>
2021-06-25 07:40:17 +0200

    Added translation using Weblate (French)

---
+++ core/components/langsuggest/lexicon/fr/default.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default Lexicon Entries for LangSuggest
+ *
+ * @package langsuggest
+ * @subpackage lexicon
+ */
+$_lang['langsuggest.bar_message'] = 'Would you prefer to continue reading on our english site? Then select your language here to see the content in your language.';
+$_lang['langsuggest.redirect_message'] = 'Do you prefer being redirected to our English language homepage?';
+$_lang['langsuggest.redirect_title'] = 'A brief question …';
