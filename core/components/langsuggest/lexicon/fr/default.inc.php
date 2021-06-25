a0f1124cb469e397278fb642512070e6ab71df38
Thomas Jakobi <buero@treehillstudio.de>
2021-06-25 05:43:24 +0000

    Translated using Weblate (French)
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/fr/

--- core/components/langsuggest/lexicon/fr/default.inc.php
+++ core/components/langsuggest/lexicon/fr/default.inc.php
@@ -5,6 +5,6 @@
  * @package langsuggest
  * @subpackage lexicon
  */
-$_lang['langsuggest.bar_message'] = 'Would you prefer to continue reading on our english site? Then select your language here to see the content in your language.';
-$_lang['langsuggest.redirect_message'] = 'Do you prefer being redirected to our English language homepage?';
-$_lang['langsuggest.redirect_title'] = 'A brief question …';
+$_lang['langsuggest.bar_message'] = 'Vous préférez continuer à lire sur notre site français ? Sélectionnez alors votre langue ici pour voir le contenu dans votre langue.';
+$_lang['langsuggest.redirect_message'] = 'Préférez-vous être redirigé vers notre page d\'accueil en français ?';
+$_lang['langsuggest.redirect_title'] = 'Une brève question …';
