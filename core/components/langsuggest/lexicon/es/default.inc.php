0714fe9226aff3cd8f2b0dacf4d08be16a60f842
Weblate (bot) <hosted@weblate.org>
2021-06-25 07:53:21 +0200

    Translations update from Weblate (#6)
    * Added translation using Weblate (Italian)
    
    * Added translation using Weblate (French)
    
    * Added translation using Weblate (Spanish)
    
    * Translated using Weblate (English)
    
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/en/
    
    * Translated using Weblate (Italian)
    
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/it/
    
    * Translated using Weblate (French)
    
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/fr/
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/es/
    
    * Update default.inc.php
    
    Co-authored-by: Thomas Jakobi <buero@treehillstudio.de>
    Co-authored-by: Thomas Jakobi <thomas.jakobi@partout.info>

---
+++ core/components/langsuggest/lexicon/es/default.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default Lexicon Entries for LangSuggest
+ *
+ * @package langsuggest
+ * @subpackage lexicon
+ */
+$_lang['langsuggest.bar_message'] = '¿Prefiere seguir leyendo en nuestra página en español? Entonces seleccione su idioma aquí para ver el contenido en su idioma.';
+$_lang['langsuggest.redirect_message'] = '¿Prefiere ser redirigido a nuestra página web en español?';
+$_lang['langsuggest.redirect_title'] = 'Una breve pregunta …';
---
+++ core/components/langsuggest/lexicon/it/default.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default Lexicon Entries for LangSuggest
+ *
+ * @package langsuggest
+ * @subpackage lexicon
+ */
+$_lang['langsuggest.bar_message'] = 'Preferisci continuare a leggere sul nostro sito italiano? Allora seleziona qui la tua lingua per vedere il contenuto nella tua lingua.';
+$_lang['langsuggest.redirect_title'] = 'Una breve domanda …';
+$_lang['langsuggest.redirect_message'] = 'Preferisci essere reindirizzato alla nostra homepage in lingua italiana?';
