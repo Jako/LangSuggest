254714962f539c7f341fcdcb3c3d7efd9a484d05
Thomas Jakobi <buero@treehillstudio.de>
2021-06-25 05:47:24 +0000

    Translated using Weblate (Italian)
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: MODX Extras/MODX LangSuggest/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-standard/it/

--- core/components/langsuggest/lexicon/it/default.inc.php
+++ core/components/langsuggest/lexicon/it/default.inc.php
@@ -1,3 +1,10 @@
 <?php
-$_lang['langsuggest.redirect_title'] = 'A brief question …';
-$_lang['langsuggest.redirect_message'] = 'Do you prefer being redirected to our English language homepage?';
+$_lang['langsuggest.redirect_title'] = 'Una breve domanda …';
+$_lang['langsuggest.redirect_message'] = 'Preferisci essere reindirizzato alla nostra homepage in lingua italiana?';
+/**
+ * Default Lexicon Entries for LangSuggest
+ *
+ * @package langsuggest
+ * @subpackage lexicon
+ */
+$_lang['langsuggest.bar_message'] = 'Preferisci continuare a leggere sul nostro sito italiano? Allora seleziona qui la tua lingua per vedere il contenuto nella tua lingua.';
