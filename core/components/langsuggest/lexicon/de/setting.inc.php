ffadd6c3a36379a1d81115cd6627929f94c90bb2
Weblate (bot) <hosted@weblate.org>
2021-11-12 12:51:08 +0100

    Translated using Weblate (German) (#9)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: MODX Extras/MODX LangSuggest/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-langsuggest-system-settings/de/
    
    Co-authored-by: Thomas Jakobi <buero@treehillstudio.de>

--- core/components/langsuggest/lexicon/de/setting.inc.php
+++ core/components/langsuggest/lexicon/de/setting.inc.php
@@ -8,7 +8,7 @@
 $_lang['setting_langsuggest.chunk_position_desc'] = 'Die Position der Ausgabe des LangSuggest-Chunks im Body-Bereich der gerenderten Seite. Kann auf \'top\' und \'bottom\' gesetzt werden.';
 $_lang['setting_langsuggest.chunk_position'] = 'Chunk-Position';
 $_lang['setting_langsuggest.cookie_expiration'] = 'Cookie-Ablauf';
-$_lang['setting_langsuggest.cookie_expiration_desc'] = 'Die Anzahl der Tage, die der LangSuggest nicht sichtbar ist, wenn er durch die Schaltfläche geschlossen wird';
+$_lang['setting_langsuggest.cookie_expiration_desc'] = 'Die Anzahl der Tage, die der LangSuggest nicht sichtbar ist, wenn er durch die Schaltfläche geschlossen wird.';
 $_lang['setting_langsuggest.cookie_name'] = 'Cookie Name';
 $_lang['setting_langsuggest.cookie_name_desc'] = 'Der Name des Cookies, das von LangSuggest festgelegt wird, wenn die Sprache gewechselt werden soll.';
 $_lang['setting_langsuggest.debug'] = 'Debug';
@@ -16,6 +16,6 @@ $_lang['setting_langsuggest.debug_desc'] = 'Log debug information in MODX error 
 $_lang['setting_langsuggest.display_count'] = 'Anzahl der Meldungen';
 $_lang['setting_langsuggest.display_count_desc'] = 'Maximale Anzahl der Meldungen des LangSuggest-Modals für jede Sitzung, wenn eine PHP-Sitzung existiert.';
 $_lang['setting_langsuggest.no_session'] = 'Sessionslose Kontexte';
-$_lang['setting_langsuggest.no_session_desc'] = 'LangSuggest in sessionslosen Kontexten anzeigen';
+$_lang['setting_langsuggest.no_session_desc'] = 'LangSuggest in sessionslosen Kontexten anzeigen.';
 $_lang['setting_langsuggest.tpl'] = 'Modal Template';
 $_lang['setting_langsuggest.tpl_desc'] = 'Der Template-Chunk, der das LangSuggest-Modal enthält.';
