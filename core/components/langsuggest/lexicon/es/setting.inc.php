c8779716b7ecac24249c1470630930dc693d9592
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:16 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/langsuggest/lexicon/es/setting.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Setting Lexicon Entries for LangSuggest
+ *
+ * @package langsuggest
+ * @subpackage lexicon
+ */
+$_lang['setting_langsuggest.chunk_position'] = '';
+$_lang['setting_langsuggest.chunk_position_desc'] = '';
+$_lang['setting_langsuggest.cookie_expiration'] = '';
+$_lang['setting_langsuggest.cookie_expiration_desc'] = '';
+$_lang['setting_langsuggest.cookie_name'] = '';
+$_lang['setting_langsuggest.cookie_name_desc'] = '';
+$_lang['setting_langsuggest.debug'] = '';
+$_lang['setting_langsuggest.debug_desc'] = '';
+$_lang['setting_langsuggest.display_count'] = '';
+$_lang['setting_langsuggest.display_count_desc'] = '';
+$_lang['setting_langsuggest.no_session'] = '';
+$_lang['setting_langsuggest.no_session_desc'] = '';
+$_lang['setting_langsuggest.tpl'] = '';
+$_lang['setting_langsuggest.tpl_desc'] = '';
